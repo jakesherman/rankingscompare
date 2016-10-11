c4a158cb695a7e7fd831f08538fe6a78fb252491
Jake Sherman <jake.sherman@digitaslbi.com>
2016-10-11 13:53:39 -0400

    Updating README

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # rankingscompare
-Rank correlation and similarity measures
+
+Rank correlation and similarity measures. The following are implemented:
+
+* Spearman's rho (ρ)
+* Kendalls' tau (τ)
+* AP correlation (top-weighted alteration of τ)
+
+To be implemented:
+
+* Rank-biased overlap (RBO)
+
+## Install
+
+```bash
+pip install git+https://github.com/jakesherman/rankingscompare.git
+```
+
+## To run unit tests
+
+```
+git clone https://github.com/jakesherman/rankingscompare.git
+cd rankingscompare
+python -m unittest tests.tau_tests
+```
--- tests/tau_tests.py
+++ tests/tau_tests.py
@@ -25,13 +25,13 @@ def approx_equal(num1, num2, significant = 2):
         return False
 
 
-def same_kendall_tau_values():
+def same_tau_a_values():
     """Compares the results of my Kendall Tau implemention with the scipy
     implementation to make sure that my results are correct for some test cases.
     """
     successes = 0
     for test_case in [func() for func in [generate_test_case] * 100]:
-        mykt, scipykt = rc.kendall_tau(*test_case), kendalltau(*test_case)[0]
+        mykt, scipykt = rc.tau_a(*test_case), kendalltau(*test_case)[0]
         if not approx_equal(mykt, scipykt):
             print 'Different values (my, scipy): {0} != {1}'.format(
                 mykt, scipykt)
@@ -49,29 +49,29 @@ class TauTestCase(unittest.TestCase):
     d, e = [5, 4, 3, 2, 1], [2, 3, 4, 5, 6]
 
     # Test for non-negativity, should be 1
-    def test_kendall_tau_1(self):
-        self.assertTrue(rc.kendall_tau(self.a, self.a) == 1.0)
+    def test_tau_a_1(self):
+        self.assertTrue(rc.tau_a(self.a, self.a) == 1.0)
 
     # Complete opposite, should be -1
-    def test_kendall_tau_2(self):
-        self.assertTrue(rc.kendall_tau(self.a, self.d) == -1.0)
+    def test_tau_a_2(self):
+        self.assertTrue(rc.tau_a(self.a, self.d) == -1.0)
 
     # Should both be 0.8
-    def test_kendall_tau_3(self):
-        self.assertTrue((rc.kendall_tau(self.a, self.b) == 0.8 and
-        rc.kendall_tau(self.a, self.c) == 0.8))
+    def test_tau_a_3(self):
+        self.assertTrue((rc.tau_a(self.a, self.b) == 0.8 and
+        rc.tau_a(self.a, self.c) == 0.8))
 
     # Should raise an AssertionError for non-conjoint lists
-    def test_kendall_tau_4(self):
-        self.assertRaises(AssertionError, rc.kendall_tau, self.a, self.e)
+    def test_tau_a_4(self):
+        self.assertRaises(AssertionError, rc.tau_a, self.a, self.e)
 
     # Should raise an AssertionError for non-unique lists
-    def test_kendall_tau_5(self):
-        self.assertRaises(AssertionError, rc.kendall_tau, [1, 1, 2], [1, 2, 2])
+    def test_tau_a_5(self):
+        self.assertRaises(AssertionError, rc.tau_a, [1, 1, 2], [1, 2, 2])
 
     # Agreement between myself and scipy
-    def test_same_kendall_tau_values(self):
-        self.assertTrue(same_kendall_tau_values())
+    def test_same_tau_a_values(self):
+        self.assertTrue(same_tau_a_values())
 
 
 if __name__ == '__main__':
